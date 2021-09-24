bdfd9a944328c609122b55223e48b4e632e1beff
starchPrince <sebume@yandex.ru>
2021-09-24 15:46:14 +0300

    dz8

---
+++ snake_l8_BulanovS/snake/Apple.swift
@@ -0,0 +1,31 @@
+//
+//  Apple.swift
+//  snake
+//
+//  Created by Сергей Буланов on 24.09.2021.
+//
+
+import UIKit
+import SpriteKit
+
+class Apple: SKShapeNode {
+    init(position: CGPoint) {
+        super.init()
+        
+        path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 10, height: 10)).cgPath
+        fillColor = .green
+        strokeColor = .green
+        lineWidth = 5
+        
+        self.position = position
+        
+        self.physicsBody = SKPhysicsBody(circleOfRadius: 10.0, center: CGPoint(x: 5, y: 5))
+        
+        
+        self.physicsBody?.categoryBitMask = CollisionCategory.Apple
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
